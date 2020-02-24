7dfae5f71cd60419bc64de05c0f6bbe6826875a2
alex <alex92471@gmail.com>
2020-02-24 23:04:56 +0300

    HW corrected

--- src/test/java/LoginProccess/UserPage.java
+++ src/test/java/LoginProccess/UserPage.java
@@ -2,26 +2,40 @@ package LoginProccess;
 
 import MoneyTransfer.TransferPage;
 import com.codeborne.selenide.ElementsCollection;
-import com.codeborne.selenide.SelenideElement;
-import lombok.Getter;
 import lombok.Value;
+import lombok.val;
 
+import java.util.ArrayList;
+
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 
 @Value
 public class UserPage {
-    private String header;
-    private ElementsCollection account = $$("li.list__item");
+    private String header = "Личный кабинет";
+    private ElementsCollection accountsList = $$("li.list__item");
 
-    public UserPage() {
-        this.header = "Личный кабинет";
+    public static TransferPage moneyTransfer(int accountIndex) {
+        UserPage userPage = new UserPage();
+        ArrayList<Integer> accountsBalances = new ArrayList<>();
+        for (int i = 0; i < userPage.getAccountsList().size() ; i++) {
+            accountsBalances.add(userPage.getAccountBalance(i));
+        }
+        userPage.getAccountsList().get(accountIndex).find(withText("Пополнить")).click();
+        return new TransferPage(accountIndex, accountsBalances);
     }
 
-    public static TransferPage moneyTransfer (int accountIndex) {
-        UserPage userPage = new UserPage();
-        userPage.getAccount().get(accountIndex).find(withText("Пополнить")).click();
-        return new TransferPage(accountIndex);
+    public int getAccountBalance(int accountIndex){
+        val accountBalanceString = getAccountsList().get(accountIndex).getText();
+        val accountBalanceSub = accountBalanceString.substring(accountBalanceString.indexOf("баланс:") + 8, accountBalanceString.indexOf(" р."));
+        val accountBalance = Integer.parseInt(accountBalanceSub);
+        return accountBalance;
+    }
+
+
+    public void verifySuccessfulLogin() {
+        $(withText(getHeader())).waitUntil(visible, 10000);
     }
 }
--- src/test/java/MoneyTransfer/TransferPage.java
+++ src/test/java/MoneyTransfer/TransferPage.java
@@ -6,6 +6,12 @@ import com.codeborne.selenide.SelenideElement;
 import lombok.Value;
 import lombok.val;
 
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 
 @Value
@@ -15,23 +21,33 @@ public class TransferPage {
     private SelenideElement transferTo = $("input[type='text'][disabled");
     private SelenideElement buttonToTransfer = $("button[data-test-id='action-transfer']");
     private int accountIndex;
+    private ArrayList<Integer> accountBalances;
 
-    public UserPage transferMoneyValid() {
-        // В идеале должно получать из базы данных текущую сумму и брать меньше или равную ей. Пытался парсить суммы по счетам из личного кабинета через XPath но к сожалению не получилось.
-        amountToTransfer.setValue("1000");
+    public void transferMoneyValid() {
+        val amountAvailableToTransfer = accountBalances.get(1 - accountIndex);
+        val randomAmountToTransfer = new Random().nextInt(amountAvailableToTransfer) + 1;
+
+        amountToTransfer.setValue(String.valueOf(randomAmountToTransfer));
         val sourceAccount = DataHelper.AuthInfo.getAuthInfo().getAccounts().get(1 - accountIndex);
         transferFrom.setValue(sourceAccount);
         buttonToTransfer.click();
-        return new UserPage();
+
+        UserPage userPageAfterTransfer = new UserPage();
+        $(withText(userPageAfterTransfer.getHeader())).waitUntil(visible, 5000);
+
+        userPageAfterTransfer.getAccountsList().get(1 - accountIndex).shouldHave(text(String.valueOf(amountAvailableToTransfer - randomAmountToTransfer)));
+        userPageAfterTransfer.getAccountsList().get(accountIndex).shouldHave(text(String.valueOf(accountBalances.get(accountIndex) + randomAmountToTransfer)));
     }
+
 
     public void transferMoneyInvalid() {
-        // В идеале должно получать из базы данных текущую сумму и брать больше неё. Пытался парсить суммы по счетам из личного кабинета через XPath но к сожалению не получилось.
-        amountToTransfer.setValue("1000000");
-        val sourceAccount = DataHelper.AuthInfo.getAuthInfo().getAccounts().get(1 - accountIndex);
+        val amountAvailableToTransfer = accountBalances.get(1);
+        val randomInvalidAmountToTransfer = amountAvailableToTransfer + 1 + new Random().nextInt(100000);
+
+        amountToTransfer.setValue(String.valueOf(randomInvalidAmountToTransfer));
+        val sourceAccount = DataHelper.AuthInfo.getAuthInfo().getAccounts().get(0);
         transferFrom.setValue(sourceAccount);
         buttonToTransfer.click();
+        $(withText("На Вашем счёте недостаточно средств")).waitUntil(visible, 5000);
     }
-
-
 }
